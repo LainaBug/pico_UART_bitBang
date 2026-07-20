c5c2a0cf22a292301628cbe43c2ab76ddba9d23e
LainaBug <laina@bugmail.xyz>
2026-07-20 19:40:59 -0400

    working, adding ascii functionality

--- UART_BitBang_Functions.c
+++ UART_BitBang_Functions.c
@@ -1,3 +1,31 @@
 //
 // Created by Laina Galayde on 7/15/26.
 //
+
+//This .c file is used to define all functions for uart bit banging
+
+#include <stdio.h>
+#include <pico/stdlib.h>
+#include "string.h"
+
+#define BITPERIOD 104
+
+void sendByte(int TX_PIN, uint8_t byte) {
+
+    //set tx line low for one bit period (start bit!)
+    gpio_put(TX_PIN, 0);
+    sleep_us(BITPERIOD);
+
+    //send all 8 data bits at a time (each one bit period ~104 microseconds for 9.6k baud)
+    for (int i = 0; i < 8; i++) {
+        uint8_t mask = 1;
+        bool data_bit = (byte >> i) & mask;
+        gpio_put(TX_PIN, data_bit);
+        sleep_us(BITPERIOD);
+    }
+
+    //let the line be idle (high!) for a bit period (Stop bit!)
+    gpio_put(TX_PIN, 1);
+    sleep_us(BITPERIOD);
+}
+
--- UART_bitBang_Functions.h
+++ UART_bitBang_Functions.h
@@ -6,3 +6,7 @@
 #define PICO_UART_BITBANG_UART_BITBANG_FUNCTIONS_H
 
 #endif //PICO_UART_BITBANG_UART_BITBANG_FUNCTIONS_H
+
+uint8_t asciiToDecimal(char ascii);
+bool decimalToBinary(uint8_t decimal, bool binary);
+void sendByte(int TX_PIN, uint8_t byte); //function declaration
